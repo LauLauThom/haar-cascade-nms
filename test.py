e931e4c3268a56e83bfe74c8ed5b15177251e2b5
Laurent Thomas <laurent132.thomas@laposte.net>
2021-11-25 11:30:07 +0100

    indentation and code format

--- test.py
+++ test.py
@@ -10,33 +10,35 @@ from skimage import io, img_as_ubyte # for image input and conversion
 import matplotlib.pyplot as plt      # for display of results
 
 
-## Step1: Load an image as 8-bit Grayscale for the detection with a cascade.
+#%% Step1: Load an image as 8-bit Grayscale for the detection with a cascade.
 imgURL = r"https://zenodo.org/record/2650147/files/WE00049---E001--PO01--LO001--CO6--00000000_00.01.00_00.16.00%2C000.jpg"
 img = io.imread(imgURL, as_gray=True)
 img = img_as_ubyte(img)
 #plt.axis("off")
 #plt.imshow(img, cmap="gray")
 
+#%% Load the cascade file
 haar_cascade = Detector(r'.\cascade\cascade.xml')
 #print("Cascade classifier was correctly loaded : ", not haar_cascade.empty())
 if haar_cascade.empty():
     raise Exception("Issue loading the cascade classifier, make sure it is in the subfolder above")
 
-
+#%% Run the detection
 bboxes, scores = haar_cascade.detectAndFilter(img,
-                                                scaleFactor = 1.1,
-                                                minSize = (300, 300),
-                                                maxSize = (500, 500),
-                                                score_threshold = 1,
-                                                overlap_threshold = 0.4,
-                                                nObjects=2
-                                                )
+                                              scaleFactor = 1.1,
+                                              minSize = (300, 300),
+                                              maxSize = (500, 500),
+                                              score_threshold = 1,
+                                              overlap_threshold = 0.4,
+                                              nObjects=1
+                                              )
+
 print("Detected bounding boxes (x,y,width,height)")
 print(bboxes)
 print(scores)
 
 
-
+#%% plot the detections
 import cv2
 
 imRGB = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
